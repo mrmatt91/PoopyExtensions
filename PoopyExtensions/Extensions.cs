677c88e2fad695abf3789bb20ddb40508117b0c2
Matt Damiter <mattdamiter@gmail.com>
2022-07-29 20:31:16 -0400

    Moved collection joins at the top level to a single, poopier function.

--- PoopyExtensions/Extensions.cs
+++ PoopyExtensions/Extensions.cs
@@ -7,8 +7,8 @@
             var x = 0;
             var poopyString = items switch
             {
-                IEnumerable<int> _ => string.Join(Environment.NewLine, items.Cast<int>().ToList().Select(i => string.Join(" ", items.Take(items.Count() - i + 1)))),
-                IEnumerable<string> _ => string.Join(Environment.NewLine, items.Select(w => string.Join(" ", items.Take(items.Count() - x++)))),
+                IEnumerable<int> => Poopify(items.Cast<int>().ToList().Select(i => string.Join(" ", items.Take(items.Count() - i + 1)))),
+                IEnumerable<string> => Poopify(items.Select(w => string.Join(" ", items.Take(items.Count() - x++)))),
                 _ => throw new NotImplementedException()
             };
             return poopyString;
@@ -17,13 +17,18 @@
         public static string Poopy(this int num)
         {
             var nums = Enumerable.Range(1, num).ToList();
-            return string.Join(Environment.NewLine, nums.Select(i => string.Join(" ", nums.Take(nums.Count() - i + 1))));
+            return Poopify(nums.Select(i => string.Join(" ", nums.Take(nums.Count() - i + 1)));
         }
 
         public static string Poopy(this string str)
         {
             var x = 0;
-            return string.Join(Environment.NewLine, str.Select(s => string.Join(" ", str.Take(str.Count() - x++))));
+            return Poopify(str.Select(s => string.Join(" ", str.Take(str.Count() - x++))));
+        }
+
+        private static string Poopify<T>(IEnumerable<T> collection)
+        {
+            return string.Join(Environment.NewLine, collection);
         }
     }
 }
